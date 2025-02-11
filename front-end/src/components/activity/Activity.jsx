70f20e3660d95de86f1236ed9d3a84df7754f804
Josh Urdnb <joshua.urdanabia@gmail.com>
2025-02-11 18:18:21 +0100

    Refactor Recharts component in Activity.jsx for better design alignment.

--- front-end/src/components/activity/Activity.jsx
+++ front-end/src/components/activity/Activity.jsx
@@ -16,7 +16,7 @@ const CustomTooltip = ({ active, payload }) => {
     return null
 };
 
-CustomTooltip.propTypes = {active: PropTypes.bool, payload: PropTypes.array}
+CustomTooltip.propTypes = { active: PropTypes.bool, payload: PropTypes.array }
 
 const Activity = () => {
     const [userData, setUserData] = useState(null)
@@ -47,26 +47,74 @@ const Activity = () => {
 
     return (
         <section className='activity-container'>
-            <p>Activité quotidienne</p>
-
+            <p className='activity-title'>Activité quotidienne</p>
             <ResponsiveContainer width="100%" height={280}>
                 <BarChart
                     data={userData.sessions}
                     margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
-                    barGap={8}
-                    barCategoryGap={12}
+                    barGap={10}  // Espacement plus large
+                    barCategoryGap={15}
                 >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="day" tickFormatter={(day) => day.slice(9)} tickLine={false} />
-                    <YAxis yAxisId="kilogram" orientation="right" domain={['dataMin - 2', 'dataMax + 1']} axisLine={false} tickLine={false} tickCount={3} />
-                    <YAxis yAxisId="calories" orientation="left" hide tickCount={3} />
-                    <Tooltip content={<CustomTooltip />} />
-                    <Legend align="right" verticalAlign="top" iconType="circle" wrapperStyle={{ lineHeight: '40px' }} />
-                    <Bar yAxisId="kilogram" dataKey="kilogram" fill="#282D30" name="Poids (kg)" radius={[10, 10, 0, 0]} barSize={10} />
-                    <Bar yAxisId="calories" dataKey="calories" fill="#E60000" name="Calories brûlées (kCal)" radius={[10, 10, 0, 0]} barSize={10} />
+                    <CartesianGrid strokeDasharray="3 3" vertical={false} />
+
+                    <XAxis
+                        dataKey="day"
+                        tickFormatter={(day) => day.slice(9)}
+                        tickLine={false}
+                        stroke="#9B9EAC"
+                    />
+
+                    <YAxis
+                        yAxisId="kilogram"
+                        orientation="right"
+                        domain={['dataMin - 2', 'dataMax + 1']}
+                        axisLine={false}
+                        tickLine={false}
+                        tick={{ fill: "#9B9EAC" }} // Texte gris clair
+                        tickCount={3}
+                    />
+
+                    <YAxis
+                        yAxisId="calories"
+                        orientation="left"
+                        hide
+                        tickCount={3}
+                    />
+
+                    <Tooltip
+                        content={<CustomTooltip />}
+                        cursor={{ fill: "rgba(196, 196, 196, 0.5)" }} // Fond gris au survol
+                    />
+
+                    <Legend
+                        align="right"
+                        verticalAlign="top"
+                        iconType="circle"
+                        wrapperStyle={{ lineHeight: '24px', fontSize: 14 }}
+                        iconSize="10"
+						height={60}
+                    />
+
+                    <Bar
+                        yAxisId="kilogram"
+                        dataKey="kilogram"
+                        fill="#282D30"
+                        name="Poids (kg)"
+                        radius={[10, 10, 0, 0]}
+                        barSize={7} // Barres plus fines
+                    />
+                    <Bar
+                        yAxisId="calories"
+                        dataKey="calories"
+                        fill="#E60000"
+                        name="Calories brûlées (kCal)"
+                        radius={[10, 10, 0, 0]}
+                        barSize={7}
+                    />
                 </BarChart>
             </ResponsiveContainer>
         </section>
+
     )
 }
 
