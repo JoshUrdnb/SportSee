41afb274a7242105f2c9a377060aca79e541baca
Josh Urdnb <joshua.urdanabia@gmail.com>
2025-02-11 19:29:05 +0100

    refactor(Average): improve Recharts design alignment.

--- front-end/src/components/average/Average.jsx
+++ front-end/src/components/average/Average.jsx
@@ -2,6 +2,20 @@ import './average.scss'
 import { useEffect, useState } from "react"
 import { ApiFactory } from '../../api/factory'
 import { LineChart, Line, XAxis, Tooltip, ResponsiveContainer } from 'recharts'
+import PropTypes from 'prop-types'
+
+const CustomTooltip = ({ active, payload }) => {
+    if (active && payload && payload.length) {
+        return (
+            <div className='custom-tooltip-average' style={{ backgroundColor: "white", padding: "10px", borderRadius: "5px" }}>
+                <p style={{ color: "#000" }}>{`${payload[0].value} min`}</p>
+            </div>
+        )
+    }
+    return null
+}
+
+CustomTooltip.propTypes = { active: PropTypes.bool, payload: PropTypes.array }
 
 const Average = () => {
     const [userData, setUserData] = useState(null)
@@ -33,12 +47,12 @@ const Average = () => {
 
     return (
         <section className='average-container'>
-            <p>Durée moyenne des sessions</p>
-            <ResponsiveContainer width="100%" height={220}>
+            <p className='average-title'>Durée moyenne des <br /> sessions</p>
+            <ResponsiveContainer width="100%" height={200}>
                 <LineChart data={formattedData}>
-                    <XAxis dataKey="day" tick={{ fill: "rgba(255, 255, 255, 0.7)" }} />
-                    <Tooltip />
-                    <Line type="basis" dataKey="sessionLength" stroke="#FFF" strokeWidth={2} dot={{ r: 4 }} />
+                    <XAxis dataKey="day" tick={{ fill: "rgba(255, 255, 255, 0.7)" }}   axisLine={false} tickLine={false} />
+                    <Tooltip content={<CustomTooltip />} />
+                    <Line type="monotone" dataKey="sessionLength" stroke="#FFF" strokeWidth={2} dot={false} activeDot={{ r: 6, fill: "#FFF" }} />
                 </LineChart>
             </ResponsiveContainer>
         </section>
